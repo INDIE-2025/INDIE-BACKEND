e8ce1e33c954011f1ee7541957557f6a37e98205
doloj <dolojadur1@gmail.com>
2025-09-22 18:06:52 -0300

    security fix

--- src/main/java/indie/controllers/moduloUsuario/UsuarioController.java
+++ src/main/java/indie/controllers/moduloUsuario/UsuarioController.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,7 +36,7 @@ public class UsuarioController extends BaseController<Usuario, String> {
     private JwtUtils jwtUtils;
 
     public UsuarioController(UsuarioService usuarioService) {
-        super(usuarioService);
+        super(usuarioService); 
         this.usuarioService = usuarioService;
     }
 
